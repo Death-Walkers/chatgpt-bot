ecab95ff4bf7a308710beb460e170c0b1a43ffd4
Death-Walkers <royalashlynnofficial@gmail.com>
2024-11-25 19:03:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = environ.get("API_ID" , "2099007")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-BOT_TOKEN = environ.get("BOT_TOKEN" , "2099007:AAH1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-ADMIN = int(environ.get("ADMIN" , "1847899007"))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
-MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
+API_ID = environ.get("API_ID" , "29196607")
+API_HASH = environ.get("API_HASH" , "f1e1079dbc06f1f6dab09d5a4ae4a682")
+BOT_TOKEN = environ.get("BOT_TOKEN" , "")
+ADMIN = int(environ.get("ADMIN" , "7427294551"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1002267496129"))
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002267496129")
+MONGO_URL = environ.get("MONGO_URL" , "mongodb+srv://jiosavan:Y4wFgYdqhdR5IxLl@files.matgb.mongodb.net/?retryWrites=true&w=majority")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-1001734958816")
+    environ.get("AUTH_CHANNEL", "-1001844331919")
 )
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
